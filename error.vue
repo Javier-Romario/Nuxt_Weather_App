6ffa1e9a6793949969baa14cbcb07676b41e38d4
Javier Harford <javierharford@Javiers-MacBook-Air.local>
2024-03-15 14:39:56 +0000

    add 404 error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="h-screen flex flex-col items-center justify-center my-2 mx-5 lg:mx-20 font-serif"
+  >
+    <NuxtLink to="/">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Go home
+      </button>
+    </NuxtLink>
+    <span class="text-2xl text-bold my-4">
+      404 Error: Four Hundred and four, this page doesn't exist
+    </span>
+  </div>
+</template>
