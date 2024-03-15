d678ff692702615ac62210b4da339e1d09a76db5
Javier Harford <javierharford@Javiers-MacBook-Air.local>
2024-03-15 14:42:01 +0000

    add favicon and metadata

--- app.vue
+++ app.vue
@@ -5,3 +5,14 @@
     </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  title: "Weather App",
+  head: {
+    htmlAttrs: {
+      lang: "en",
+    },
+  },
+  meta: [{ name: "description", content: "Weather App for a tech test" }],
+});
+</script>
