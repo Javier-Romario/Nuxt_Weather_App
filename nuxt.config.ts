a35399a863ec9d34629f96e65c7c68f7a7baca51
Javier Harford <javierharford@Javiers-MacBook-Air.local>
2024-03-15 14:39:45 +0000

    add extra nuxt configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,28 @@ export default defineNuxtConfig({
     },
   },
   runtimeConfig: {
-    WEATHER_API: process.env.NUXT_WEATHER_API,
+    WEATHER_API:
+      process.env.NODE_ENV === "production"
+        ? process.env.NUXT_WEATHER_API
+        : process.env.NUXT_WEATHER_API_DEV,
+  },
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/sitemap.xml", "/robots.txt"],
+    },
+  },
+  routeRules: {
+    "/sitemap.xml": { prerender: false },
+    "/robots.txt": { prerender: false },
   },
 });
